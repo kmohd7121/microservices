d8c3408de9efdb4265f6f28d61e8e26d968d04e9
kmohd7121 <65274900+kmohd7121@users.noreply.github.com>
2021-01-18 16:39:06 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	" github.com/kmohd7121/microservices/handlers"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	http.ListenAndServe(":9090", sm)
+
+}
