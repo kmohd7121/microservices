a7e3ced686388ee8582cf36bbf82e97246ad9e51
kmohd7121 <65274900+kmohd7121@users.noreply.github.com>
2021-01-22 23:57:14 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"os/signal"
 	"time"
 
-	"gilab.com/kmohd7121/microservices/handlers"
-     " github.com/gin-gonic/gin"
+	"gilab.com/kmohd7121/microservices/product-api/handlers"
+	"github.com/kmohd7121/env"
 )
 
 var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
@@ -19,25 +19,19 @@ func main() {
 	env.Parse()
 
 	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
-
-	// create the handlers
 	ph := handlers.NewProducts(l)
-
-	// create a new serve mux and register the handlers
 	sm := http.NewServeMux()
 	sm.Handle("/", ph)
 
-	// create a new server
 	s := http.Server{
-		Addr:         *bindAddress,      // configure the bind address
-		Handler:      sm,                // set the default handler
-		ErrorLog:     l,                 // set the logger for the server
-		ReadTimeout:  5 * time.Second,   // max time to read request from the client
-		WriteTimeout: 10 * time.Second,  // max time to write response to the client
-		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+		Addr:         *bindAddress,
+		Handler:      sm,
+		ErrorLog:     l,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
 	}
 
-	// start the server
 	go func() {
 		l.Println("Starting server on port 9090")
 
@@ -48,16 +42,11 @@ func main() {
 		}
 	}()
 
-	// trap sigterm or interupt and gracefully shutdown the server
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	signal.Notify(c, os.Kill)
-
-	// Block until a signal is received.
 	sig := <-c
 	log.Println("Got signal:", sig)
-
-	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	s.Shutdown(ctx)
 }
